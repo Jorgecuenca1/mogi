0f3b17d460b6de52acc3e152995924de11940814
pspedro19 <96pedroelias96@gmail.com>
2024-02-13 17:37:37 -0500

    up project

--- mogi/settings.py
+++ mogi/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mogi.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -76,13 +76,22 @@ WSGI_APPLICATION = 'mogi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'culturameta_db',
+        'USER': 'culturameta',
+        'PASSWORD': 'CultuR4M3t4',
+        'HOST': 'database',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
