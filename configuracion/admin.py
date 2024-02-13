fd6a929403e3748506876a92defaa88cfd5f1d3f
pspedro19 <96pedroelias96@gmail.com>
2024-02-13 18:10:51 -0500

    up project

--- configuracion/admin.py
+++ configuracion/admin.py
@@ -2,10 +2,7 @@ from .models import Pagina, Noticia, SubMenu, Menu, Document, Slider
 from django.contrib import admin
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
-from django.contrib import admin
-from django.views.decorators.csrf import csrf_exempt
 
-@csrf_exempt
 class DocumentInline(admin.TabularInline):
     model = Document
 class SubMenuInline(admin.TabularInline):
