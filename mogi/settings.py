15aa742e6ab34ae289b85fa32adf0edaef2235bf
pspedro19 <96pedroelias96@gmail.com>
2024-02-13 17:59:50 -0500

    up project

--- mogi/settings.py
+++ mogi/settings.py
@@ -47,11 +47,15 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
+CORS_ALLOWED_ORIGINS = [
+    "https://mogi.vueanalytic.com",
+    # Otros dominios permitidos, si es necesario
+]
 ROOT_URLCONF = 'mogi.urls'
 
 TEMPLATES = [
